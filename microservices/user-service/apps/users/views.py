6158bee1f9d2f78d2b4bb32a38eb3ffde305c387
anapty-xi <mironovn2006@gmail.com>
2025-11-06 11:58:50 +0300

    microservices.user-service.appa.users login_view add

--- microservices/user-service/apps/users/views.py
+++ microservices/user-service/apps/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response(
+            {'error': 'Юзернейм и пароль необходимы для входа'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    user = authenticate(username=username, password=password)
+    if user and user.is_active:
+        refresh = RefreshToken.for_user(user)
+        return Response ({
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+            'user': {
+                'id': user.id,
+                'username': user.username,
+            }
+        })
+    return Response(
+        {'error', 'Invalid credentials'},
+        status=status.HTTP_401_UNAUTHORIZED
+    )
